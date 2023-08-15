774f8eb9e78b3e6cc7fdfa92135d64257f3a3570
KemalAtdayew <kemalatdayew18@gmail.com>
2023-08-16 02:25:27 +0500

    registration date now generated in pgsql by default

--- sql/users.sql
+++ sql/users.sql
@@ -1,9 +1,8 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
-    password VARCHAR(100) NOT NULL,
     blocked BOOLEAN NOT NULL DEFAULT false,
-    registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    registration_date TIMESTAMP DEFAULT TO_TIMESTAMP(TO_CHAR(CURRENT_TIMESTAMP, 'DD-MM-YYYY HH24:MI:SS'), 'DD-MM-YYYY HH24:MI:SS')
 );
